ec9de5d98c08476d15e7c36ab040b7393716805b
Kirat Gill <kiratsgill@outlook.com>
2023-10-31 13:55:42 -0500

    Ganache testing completed, testnet testing started

--- blockchain/data_trading/truffle-config.js
+++ blockchain/data_trading/truffle-config.js
@@ -1,11 +1,23 @@
+require('dotenv').config(); // at the top of your truffle-config.js
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const infuraKey = "a7b4fad1622c4be3891fe5dce5677e13";
+
+// Use process.env to access the environment variable
+const mnemonic = process.env.MNEMONIC;
+
 module.exports = {
   networks: {
-    development: {
-      host: "localhost",
-      port: 7545,
-      network_id: "*", // Match any network id
-      gas: 5000000
-    }
+    goerli: {
+      provider: () => new HDWalletProvider(mnemonic, `https://goerli.infura.io/v3/${infuraKey}`),
+      network_id: 5,       // Goerli's network id
+      gas: 5500000,        // Gas limit - How much gas is allowed for the deployment
+      confirmations: 2,    // Number of confirmations to wait between deployments (the higher the more secure)
+      timeoutBlocks: 200,  // Number of blocks before a deployment times out
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets)
+    },
+  },
+  develop: {
+    port: 8545
   },
   compilers: {
     solc: {
